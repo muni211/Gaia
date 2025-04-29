8d7c6bac64631e76e70b6746771b865879d95554
muni211 <muni5576@gmail.com>
2025-04-29 15:02:53 +0300

    Update nlp_module.js

--- nlp/nlp_module.js
+++ nlp/nlp_module.js
@@ -1,6 +1,5 @@
-// מודול NLP שעושה עיבוד טקסט בסיסי – לדוגמה, ממיר את הטקסט לאותיות גדולות
 const NLP = {
   process: function(text) {
-    return text.toUpperCase();
+    return text.toUpperCase();  // שינוי הטקסט לאותיות גדולות
   }
 };
